d615697358ec0b0326969ed97d7f6e2f55a7af1e
Sergei Potekhin <vexilurz@gmail.com>
2023-07-12 20:43:57 +0300

    Connect input text and label

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState('some text');
 
   function inc() {
     setCount(count + 1)
@@ -16,6 +17,13 @@ function App() {
       <h1>{count}</h1>
       <button onClick={inc}>Inc</button>
       <button onClick={dec}>Dec</button>
+
+      <h1>{value}</h1>
+      <input 
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
     </div>
   );
 }
