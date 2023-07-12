0c53535194c7dd1f3370c057daf8c4fd9c1fae50
Sergei Potekhin <vexilurz@gmail.com>
2023-07-12 22:48:27 +0300

    PostsList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './styles/App.css';
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,9 +12,7 @@ function App() {
 
   return (
     <div className="App">
-        {posts.map(post =>
-            <PostItem post={post} key={post.id} />
-        )}
+        <PostList posts={posts}/>
     </div>
   );
 }
