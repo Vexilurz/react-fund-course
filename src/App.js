7048ed6236f1c3c47d880b4e12632e215afd60b5
Sergei Potekhin <vexilurz@gmail.com>
2023-07-12 20:40:26 +0300

    Count example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function inc() {
+    setCount(count + 1)
+  }
+  
+  function dec() {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      
+      <h1>{count}</h1>
+      <button onClick={inc}>Inc</button>
+      <button onClick={dec}>Dec</button>
     </div>
   );
 }
