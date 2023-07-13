2afc942a58eb6d1546779ebef5027ae1b559c665
Sergei Potekhin <vexilurz@gmail.com>
2023-07-13 16:23:23 +0300

    No posts found text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
 
 function App() {
@@ -23,11 +21,15 @@ function App() {
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList
-        remove={removePost}
-        posts={posts}
-        title="Posts list 1"
-      />
+      {posts.length !== 0 ? (
+        <PostList
+          remove={removePost}
+          posts={posts}
+          title="Posts list 1"
+        />
+      ) : (
+        <h1 style={{ textAlign: 'center' }}>No posts found</h1>
+      )}
     </div>
   );
 }
