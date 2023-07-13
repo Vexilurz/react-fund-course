31ef7688bd6f252104e58b0b906c96b44d1c9ded
Sergei Potekhin <vexilurz@gmail.com>
2023-07-13 16:06:53 +0300

    PostForm component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,35 +12,13 @@ function App() {
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ]);
 
-  const [post, setPost] = useState({ title: '', body: '' });
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost = {
-      ...post,
-      id: Date.now(),
-    };
+  const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setPost({ title: '', body: '' });
   };
 
   return (
     <div className="App">
-      <form className="create-post-form">
-        <MyInput
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          type="text"
-          placeholder="Post name"
-        />
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-          type="text"
-          placeholder="Post description"
-        />
-        <MyButton onClick={addNewPost}>Create post</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList
         posts={posts}
         title="Posts list 1"
