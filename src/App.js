79f0b24aa4e2c98b7a054059b252713ce7547913
Sergei Potekhin <vexilurz@gmail.com>
2023-07-13 22:02:04 +0300

    Start search realization (badly now)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './styles/App.css';
+import MyInput from './components/UI/input/MyInput';
 import MySelect from './components/UI/select/MySelect';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
@@ -11,6 +12,19 @@ function App() {
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ]);
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function getSortedPosts() {
+    console.log('getSortedPosts()');
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts();
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -22,24 +36,34 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <MySelect
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue="Sort by"
-        options={[
-          { value: 'title', name: 'By title' },
-          { value: 'body', name: 'By content' },
-        ]}
-      />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search..."
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sort by"
+          options={[
+            { value: 'title', name: 'By title' },
+            { value: 'body', name: 'By content' },
+          ]}
+        />
+      </div>
       {posts.length !== 0 ? (
-        <PostList remove={removePost} posts={posts} title="Posts list 1" />
+        <PostList
+          remove={removePost}
+          posts={sortedPosts}
+          title="Posts list 1"
+        />
       ) : (
         <h1 style={{ textAlign: 'center' }}>No posts found</h1>
       )}
