a12b0c20467eae7b28db0da6d3207b330a87ad1e
Sergei Potekhin <vexilurz@gmail.com>
2023-07-13 16:18:58 +0300

    Delete post

--- src/App.js
+++ src/App.js
@@ -16,10 +16,15 @@ function App() {
     setPosts([...posts, newPost]);
   };
 
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
       <PostList
+        remove={removePost}
         posts={posts}
         title="Posts list 1"
       />
