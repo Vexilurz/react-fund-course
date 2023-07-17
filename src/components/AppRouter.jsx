b7439f1fd853efed624c715eef3936003c411d34
Sergei Potekhin <vexilurz@gmail.com>
2023-07-17 14:24:18 +0300

    routes to separate file

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
-import PostIdPage from '../pages/PostIdPage';
+import { routes } from '../router/routes';
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/about" element={<About />} />
-      <Route exact path="/posts" element={<Posts />} />
-      <Route exact path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/error" element={<Error />} />
-      <Route path="*" element={<Navigate to="/error" replace />} />
+      {routes.map((route) => (
+        <Route
+          path={route.path}
+          element={route.component}
+          exact={route.exact}
+        />
+      ))}
+      <Route path="*" element={<Navigate to="/posts" replace />} />
     </Routes>
   );
 }
