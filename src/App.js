1431fb2462b4d55226b200c10206d52c57cac099
Sergei Potekhin <vexilurz@gmail.com>
2023-07-17 13:37:42 +0300

    AppRouter component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import About from './pages/About';
 import Posts from './pages/Posts';
 import Error from './pages/Error';
 import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/AppRouter';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="*" element={<Navigate to="/error" replace />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 }
